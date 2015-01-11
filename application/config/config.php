8a34720a5126845757a4ecd9c55cd076e209a68d
Matt Gerstman <mattgerstman@gmail.com>
2015-01-11 10:45:04 -0800

    Ignore oauth file

--- application/config/config.php
+++ application/config/config.php
@@ -39,7 +39,7 @@
 | path to your installation.
 |
 */
- $config['base_url']	= 'http://192.168.59.103/TweetTwoScreens';
+ $config['base_url']	= 'http://apps.mattgerstman.com/TweetTwoScreens/';
 
 /*
 |--------------------------------------------------------------------------
